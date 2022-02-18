47dbe279b44024967b9399e012e168258b5c2333
Thom Stricklin <thom@thomshouse.net>
2022-02-18 14:40:09 -0700

    Fix Windows home folder redirect logic

--- ellipsis.sh
+++ ellipsis.sh
@@ -8,8 +8,8 @@ pkg.install() {
             # Ask to add the init script to each valid file
             # Add to the script if selected
             echo -e '\n# Redirect from WSL default directory since linux fs is faster' >> "$HOME/$rcfile"
-            echo -e 'pwd | grep -e "^/mnt/./Users/[^/]+?$" >/dev/null 2>&1 && cd' >> "$HOME/$rcfile"
-            echo -e 'echo "$OLDPWD" | grep -e "^/mnt/./Users/[^/]+?$" >/dev/null 2>&1 && cd\n' >> "$HOME/$rcfile"
+            echo -e 'pwd | grep -E "^/mnt/./Users/[^/]+?$" >/dev/null 2>&1 && cd' >> "$HOME/$rcfile"
+            echo -e 'echo "$OLDPWD" | grep -E "^/mnt/./Users/[^/]+?$" >/dev/null 2>&1 && cd\n' >> "$HOME/$rcfile"
         fi
     done
 
